a282c476529f309af935d8641dd0bef527af49e3
Fumiya Kinokawa <aqua.fk0324@gmail.com>
2022-02-12 01:08:58 +0900

    JSON追加

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), { // #mapに地図を埋め込む
     center: mapCenter, // 地図の表示中心地を設定（今回は緯度経度を変数に格納しそれを呼び出し）
     zoom: 12, // 地図のズームを指定
+    mapId: '6f99372f7c64b8b1', // MapIDの使用
     mapTypeControl: false, // マップ切り替えのコントロールを表示するかどうか
     streetViewControl: true // ストリートビューのコントロールを表示するかどうか
   });
@@ -36,6 +37,37 @@ function initMap() {
   });
 */
 
+// Transit Layer（おまけ）
+  const transitLayer = new google.maps.TransitLayer();
+  transitLayer.setMap(map);
+
+
+// マーカー配置の準備　（①JSONを呼びに行き、ドキュメントに配架）
+  const script = document.createElement("script");
+  script.setAttribute("src", "../js/package.json");
+  document.getElementsByTagName("head")[0].appendChild(script);
+
+// 呼び出し
+  window.feed_callback = function(results) {
+      //マップにマーカーを生成
+      for (var i = 0; i < results.features.length; i++) {
+        var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1],coords[0]);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
+        });
+        //吹き出しの中身の文言を引数で送る。
+        attachMessage(marker, results.features[i].properties.name);
+      }
+    }
+    //マーカーをクリックしたときに、吹き出しを出す。
+    function attachMessage(marker, msg) {
+      google.maps.event.addListener(marker, 'click', function(event) {
+        new google.maps.InfoWindow({
+          content: msg
+        }).open(marker.getMap(), marker);
+      });
 /*
   // マーカー
   marker = new google.maps.Marker({ // マーカーの追加
